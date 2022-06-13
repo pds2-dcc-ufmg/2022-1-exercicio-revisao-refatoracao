9669c6932010c52425f99b731c192a8b5051b749
RubiaSozua <rubia.alice.m.s@gmail.com>
2022-06-13 19:35:17 -0300

    feat(Alimentação): Adiciona sets e gets

--- src/classes/Alimentacao/Alimentacao.cpp
+++ src/classes/Alimentacao/Alimentacao.cpp
@@ -4,10 +4,10 @@
 
 void Alimentacao::print() {
     std::cout << "Animal: ";
-    std::cout << nomeAnimal << "\n";
+    std::cout << getAnimalName() << "\n";
 
     std::cout << " Cuidador: ";
-    std::cout << cuid.getName() << "\n";
+    std::cout << getResponsable().getName() << "\n";
 
     return;
     // TODO: Deletar?
@@ -23,3 +23,48 @@ void Alimentacao::print() {
     std::cout << " Animal: ";
     cout << alimentado;
 }*/
+
+void Alimentacao::setResponsable(const Cuidador responsable) {
+    this->responsable = responsable;
+    return;
+}
+
+Cuidador Alimentacao::getResponsable() const {
+    return responsable;
+}
+
+void Alimentacao::setFoodType(const std::string foodType) {
+    this->foodType = foodType;
+    return;
+}
+
+std::string Alimentacao::getFoodType() const {
+    return foodType;
+}
+
+void Alimentacao::setAmount(const unsigned int amount) {
+    this->amount = amount;
+    return;
+}
+
+unsigned int Alimentacao::getAmount() const {
+    return amount;
+}
+
+void Alimentacao::setDescription(const std::string description) {
+    this->description = description;
+    return;
+}
+
+std::string Alimentacao::getDescription() const {
+    return description;
+}
+
+void Alimentacao::setAnimalName(const std::string animalName) {
+    this->animalName = animalName;
+    return;
+}
+
+std::string Alimentacao::getAnimalName() const {
+    return animalName;
+}
--- src/classes/Alimentacao/Alimentacao.hpp
+++ src/classes/Alimentacao/Alimentacao.hpp
@@ -6,15 +6,30 @@
 #include "../Cuidador/Cuidador.hpp"
 
 class Alimentacao {
-    public:
-        Cuidador cuid;
-        std::string comida;
-        unsigned int porcao;
-        std::string descricao;
-        std::string nomeAnimal;
+    private:
+        Cuidador responsable;
+        std::string foodType;
+        unsigned int amount;
+        std::string description;
+        std::string animalName;
 
     public:
         void print();
+
+        void setResponsable(const Cuidador responsable);
+        Cuidador getResponsable() const;
+
+        void setFoodType(const std::string foodType);
+        std::string getFoodType() const;
+
+        void setAmount(const unsigned int amount);
+        unsigned int getAmount() const;
+
+        void setDescription(const std::string description);
+        std::string getDescription() const;
+
+        void setAnimalName(const std::string animalName);
+        std::string getAnimalName() const;
 };
 
 #endif
