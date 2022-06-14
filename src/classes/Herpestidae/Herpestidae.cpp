3de210c2333b4d9853e6a86380130506b411c236
RubiaSozua <rubia.alice.m.s@gmail.com>
2022-06-13 23:33:59 -0300

    feat(Herpestidae): Adiciona sobrecarga de construtores

--- src/classes/Herpestidae/Herpestidae.cpp
+++ src/classes/Herpestidae/Herpestidae.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 
 #include "./Herpestidae.hpp"
+#include "../Animal/Animal.hpp"
 
 Herpestidae::Herpestidae() {
-    setFamily("Herpestidae");
-    deit.setPortionsSize(PORTIONS_SIZE);
+    setDefaultInfo();
+}
+
+Herpestidae::Herpestidae(const std::string name) : Animal(name) {
+    setDefaultInfo();
+}
+
+Herpestidae::Herpestidae(const std::string name, const unsigned int age) : Animal(name, age) {
+    setDefaultInfo();
+}
+
+Herpestidae::Herpestidae(const std::string name, const unsigned int age, const std::string species) : Animal(name, age, species) {
+    setDefaultInfo();
 }
 
 Herpestidae::~Herpestidae() {}
+
+void Herpestidae::setDefaultInfo() {
+    setFamily("Herpestidae");
+    deit.setPortionsSize(PORTIONS_SIZE);
+}
--- src/classes/Herpestidae/Herpestidae.hpp
+++ src/classes/Herpestidae/Herpestidae.hpp
@@ -10,7 +10,13 @@ class Herpestidae : public Animal {
         static const unsigned int PORTIONS_SIZE = 1;
 
         Herpestidae();
+        Herpestidae(const std::string name);
+        Herpestidae(const std::string name, const unsigned int age);
+        Herpestidae(const std::string name, const unsigned int age, const std::string species);
         virtual ~Herpestidae();
+    
+    private:
+        void setDefaultInfo();
 };
 
 #endif
