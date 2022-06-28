73deb2f29c9df2f674f732b02c66bae51cd2ccee
Matheus Grandinetti <94018679+MFreyaG@users.noreply.github.com>
2022-06-28 15:33:50 -0300

    Update Herpestidae.hpp
    - Formatação do código usando Clang-Format para C++.
    - Retirada da variável quantPORCAO, o cálculo de informações relativas à alimentação são agora exclusivas da classe Alimentacao.
    - Override da função infoAnimal, tornando a função mais específica que na superclasse Animal.
    - Separação da especificação/implementação. Em Herpestidae.hpp apenas são declaradas as funções.

--- Herpestidae.hpp
+++ Herpestidae.hpp
@@ -1,35 +1,18 @@
-#ifndef Herpestidae_HPP
-#define Herpestidae_HPP
+#ifndef HERPESTIDAE_H
+#define HERPESTIDAE_H
 
 #include "Animal.hpp"
 
-using namespace std;
-
-double quantPORCAO = 1;
-
-
 class Herpestidae : public Animal {
+ private:
+  std::string especie;
 
-    public:
+ public:
+  Herpestidae(std::string _idade, std::string _familia, std::string _nome,
+              std::string _especie);
 
-    public:
-  string species;
-
-    double kgConsumidosDeRacao(int porcoesConsumidas) {
-        double c = quantPORCAO*porcoesConsumidas;
-                  return c;
-    }
-
-
-    void print() {
-
-        Animal::print();
-
-
-
-        std::cout << "  Especie: " << species <<endl;
-
-    }
+  // Printa informações de determinado animal da espécie Herpestidae.
+  virtual void infoAnimal() override;
 };
 
 #endif
