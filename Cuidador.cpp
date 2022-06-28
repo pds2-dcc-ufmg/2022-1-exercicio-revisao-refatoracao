1331dd783eeea50f2d6a2aa7b567dc6c31ee0653
larisaguiar <86538713+larisaguiar@users.noreply.github.com>
2022-06-28 20:59:10 -0300

    Update Cuidador.cpp

--- Cuidador.cpp
+++ Cuidador.cpp
@@ -2,13 +2,23 @@
 #include <string>
 #include "Cuidador.hpp"
 
-void Cuidador::print(){
+Cuidador :: Cuidador(string _salario, string _nome, string _cpf, string _telefone, string _nascimento) {
+	this->salario = _salario;
+	this->nome = _nome;
+	this->cpf = _cpf;
+	this->telefone = _telefone;
+	this->nascimento = _nascimento;
+}
 
+void Cuidador::print(){
 std::cout << "[Cuidador]" << std::endl
-         << "  Nome: " << NOME << std::endl
-         << "  CPF: " << cpf << std::endl
-	 << "  Telefone: " << Telefone << std::endl
-	 << "  Nascimento: " << BIRTH << std::endl;
+         << "  Nome: " << this->nome << std::endl
+         << "  CPF: " << this->cpf << std::endl
+	 << "  Telefone: " << this->telefone << std::endl
+	 << "  Nascimento: " <<this-> nascimento << std::endl;
 
 }
 
+Cuidador :: string  getNome() {
+	return this->nome;
+}
