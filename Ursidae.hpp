eadb3d653edf617d9292505352f7861aae7f7e54
gccostaa <94900894+gccostaa@users.noreply.github.com>
2022-06-30 17:41:52 -0300

    Update Ursidae.hpp

--- Ursidae.hpp
+++ Ursidae.hpp
@@ -10,7 +10,11 @@
 class Ursidae: public Animal {
   
     public:
-    
+      
+       Ursidae():Animal(std::string nome, int idade, std::string familia, std::string especie) {
+            familia = 'Ursidae';
+        }
+  
        Animal::print();
 
        Ursidae::calculaPeixesConsumidos(int porcaoPeixes) {
