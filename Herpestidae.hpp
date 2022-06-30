d96804fa223c4b2c9fd1e73384f1c12c775d0839
gccostaa <94900894+gccostaa@users.noreply.github.com>
2022-06-30 16:12:03 -0300

    Update Herpestidae.hpp
    Remoção de código repetido e troca do nome de variáveis

--- Herpestidae.hpp
+++ Herpestidae.hpp
@@ -1,34 +1,21 @@
-#ifndef Herpestidae_HPP
-#define Herpestidae_HPP
+#ifndef HERPESTIDAE_HPP
+#define HERPESTIDAE_HPP
 
+#include <iostream>
+#include <string>
 #include "Animal.hpp"
 
-using namespace std;
-
-double quantPORCAO = 1;
-
+#define QUANTPORCAO = 1
 
 class Herpestidae : public Animal {
 
     public:
-
-    public:
-  string species;
-
-    double kgConsumidosDeRacao(int porcoesConsumidas) {
-        double c = quantPORCAO*porcoesConsumidas;
-                  return c;
-    }
-
-
-    void print() {
-
+    
         Animal::print();
-
-
-
-        std::cout << "  Especie: " << species <<endl;
-
+        
+        Herpestidae::kgConsumidosDeRacao(int porcoesConsumidas) {
+        double kgDeRacao = QUANTPORCAO * porcoesConsumidas;
+                  return kgDeRacao;
     }
 };
 
