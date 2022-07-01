bedb92f4a5bc468c196269a3c6555675b4db9a2a
RafaelPChequer <73084877+RafaelPChequer@users.noreply.github.com>
2022-07-01 09:48:08 -0300

    4b912cf21b4bde6a0352c1ec7149b0b4
    -Arquivo removido, pois não tinha utilidade

--- Cuidador.cpp
+++ Cuidador.cpp
@@ -1,14 +1,1 @@
-#include <iostream>
-#include <string>
-#include "Cuidador.hpp"
-
-void Cuidador::print(){
-
-	std::cout << "[Cuidador]" << endl
-         << "  Nome: " << NOME << endl
-		 << "  CPF: " << cpf << endl
-		<< "  Telefone: " << Telefone << endl
-		 << "  Nascimento: " << BIRTH << endl;
-
-}
 
