14d9f5304e906a47368a6bed023a72d64f09fc1b
Thais <thaisfdasilva159@gmail.com>
2022-07-01 18:30:39 -0300

    Reformulação do main

--- src/main.cpp
+++ src/main.cpp
@@ -1,151 +1,63 @@
-#include <iostream>
-
-#include "Animal.hpp"
 #include "Herpestidae.hpp"
 #include "Ursidae.hpp"
 #include "Cuidador.hpp"
-#include "Alimentacao.hpp"
-using namespace std;
 
-int main()
-{
+int main(){
+    Animal animal01("Bobby","Ursidae",13);
+    animal01.print();
 
-    Animal animal1;
-    animal1.family = "Ursidae";
-    animal1.nome ="Bobby";
-	  animal1.idade = "13";
-    animal1.print();
-    double kgPeixeConsumidos01 = 0;
-    Animal animal2;
-    animal2.family = "Ursidae";
-    animal2.nome ="Jorge";
-	  animal2.idade = "1";
-    animal2.print();
+    Animal animal02("Jorge", "Ursidae",1);
+    animal02.print();
 
-    Animal animal3;
-    animal3.family = "Herpestidae";
-    animal3.nome ="Bonina";
-	  animal3.idade = "7";
-    animal3.print();
+    Animal animal03("Bonina","Herpestidae",7);
+    animal03.print();
 
-    Animal animal4;
-    animal4.family = "Ursidae";
-    animal4.nome ="Felício";
-	  animal4.idade = "3";
-    animal4.print();
+    Animal animal04("Felício", "Ursidae",3);
+    animal04.print();
 
+    Ursidae animal05("Tony", "Urso Pardo",9);
+    animal05.print();
 
+    Ursidae animal06("Wanda", "Urso Polar",10);
+    animal06.print();
 
-    Ursidae e02;
-    int numAtendimentos02 = 0;
-    double comissao02 = 0;
-    e02.nome = "Tony";
-    e02.idade = 9;
-    e02.especie = "Urso Pardo";
-    e02.kgPeixePorDIA = 10.5;
-    e02.print();
+    Herpestidae animal07("Catita", "Suricato",2);
+    animal07.print();
+    
+    Herpestidae animal08("Carolina","Suricato",2);
 
+    Cuidador cuidador01("Ana Maria Rodrigues Lopes");
+    Cuidador cuidador02("Fernanda Silva Santos");
 
-    Ursidae e03;
-    int numAtendimentos03 = 0;
-    double comissao03 = 0;
-    e03.nome = "Wanda";
-    e03.idade = 10;
-    e03.especie = "Urso Polar";
-    e03.kgPeixePorDIA = 7;
-    e03.print();
+    animal01.setTipoDescricao("Peixes","Bobby só quis comer 2 porções de peixes hoje");
+    animal01.setPorcaoQuantidadePorcao(2,5);
+    animal01.setQuantidadeConsumida(10);
+
+    animal07.setTipoDescricao("Ração especial", "Catita comeu direitinho");
+    animal07.setPorcaoQuantidadePorcao(1,1);
+    animal07.setQuantidadeConsumida(animal07.calcularQuantidadeConsumida());
+
+    animal08.setTipoDescricao("Ração", "Carolina estava com um apetite e tanto");
+    animal08.setPorcaoQuantidadePorcao(3,1);
+    animal08.setQuantidadeConsumida(animal08.calcularQuantidadeConsumida());
+
+    animal05.setTipoDescricao("Peixes", "Tony comeu um pouco da comida do Bobby");
+    animal05.setPorcaoQuantidadePorcao(10,5);
+    animal05.setQuantidadeConsumida(animal05.calcularQuantidadeConsumida());
 
 
-    Herpestidae g01;
-    g01.family = "Herpestidae";
-    g01.nome ="Catita";
-	  g01.idade = "2";
-    g01.species = "Suricato";
-    g01.print();
-    Herpestidae g02;
-    double bonificacao01 = 0;
-    g02.family = "Herpestidae";
-    g02.nome ="Carolina";
-	  g02.idade = "2";
-    g02.species = "Suricato";
+    cout <<" \n \n--------------------------------\n\nRelatorio das alimentações " << endl;
 
-    double kgPeixeConsumidos02 = 0;
-    double kgPeixeConsumidos03 = 0;
-    double kgPeixeConsumidos04 = 0;
-    double kgPeixeConsumidos05 = 0;
+    cout << "\nAnimal: " << animal01.getNome() << "\n Cuidador: " << cuidador01.getNome() << "\n Descricao: " << animal01.getDescricao() << endl;
+    cout << "\nAnimal: " << animal07.getNome() << "\n Cuidador: " << cuidador02.getNome() << "\n Descricao: " << animal07.getDescricao() << endl;
+    cout << "\nAnimal: " << animal08.getNome() << "\n Cuidador: " << cuidador02.getNome() << "\n Descricao: " << animal08.getDescricao() << endl;
+    cout << "\nAnimal: " << animal05.getNome() << "\n Cuidador: " << cuidador01.getNome() << "\n Descricao: " << animal05.getDescricao() << endl;
 
-    double kgRacaoConsumidos01 = 0;
-    double kgRacaoConsumidos02 = 0;
-    double kgRacaoConsumidos03 = 0;
-  
-    Cuidador cuid01;
-    cuid01.NOME = "Ana Maria Rodrigues Lopes";
-
-    Cuidador cuid2;
-    cuid2.NOME = "Fernanda Silva Santos";
-
-    Alimentacao v01;
-    v01.porcao= 2;
-    v01.comida = "Peixes";
-    v01.descricao = "Bobby só quis comer 2 porções de peixes hoje";
-    v01.cuid = cuid01;
-    v01.nomeAnimal = "Bobby";
-    kgPeixeConsumidos01 = 5*2;
-
-    Alimentacao v02;
-    v02.porcao= 1;
-    v02.comida = "Ração especial";
-    v02.descricao = "Catita comeu direitinho";
-    v02.cuid = cuid2;
-    v02.nomeAnimal = "Catita";
-    kgRacaoConsumidos01 += g01.kgConsumidosDeRacao(v02.porcao);
-
-    Alimentacao v03;
-    v03.porcao= 3;
-    v03.comida = "Ração";
-    v03.descricao = "Carolina estava com um apetite e tanto";
-    v03.cuid = cuid2;
-    v03.nomeAnimal = "Carolina";
-    kgRacaoConsumidos02 += g02.kgConsumidosDeRacao(v03.porcao);
-
-    Alimentacao v04;
-    v04.porcao= 10;
-    v04.comida = "Peixes";
-    v04.descricao = "Tony comeu um pouco da comida do Bobby";
-    v04.cuid = cuid01;
-    v04.nomeAnimal = "Tony";
-    v02.porcao = 2;
-    v03.comida = "Ração";
-    kgPeixeConsumidos03 = 
-    e02.calculaPeixesConsumidos(10);
-
-
-
-    cout <<" \n \n--------------------------------\n\nRelatorio das alimentações \n" << endl;
-
-    v01.print();
-    cout << " Descricao: " << v01.descricao << endl << endl;
-
-    v02.print();
-    cout << " Descricao: " << v02.descricao << endl<<endl;
-
-    v03.print();
-    cout << " Descricao: " << v03.descricao << endl<< endl;
-
-    v04.print();
-    cout << " Descricao: " << v04.descricao << endl << "\n";
-
-
-
-
-
-
-    cout <<" \n \n--------------------------------\n\nRelatorio de kg de comida gastos \n" << endl;
+    cout <<"\n \n \n--------------------------------\n\nRelatorio de kg de comida gastos \n" << endl;
     cout << "Tipo de comida: " << "Ração" << endl;
-    cout << "Kg consumidos: " << kgRacaoConsumidos01+kgRacaoConsumidos02<<endl;
-
+    cout << "Kg consumidos: " << animal07.getQuantidadeConsumida() + animal08.getQuantidadeConsumida() << endl;
     cout << "\nTipo de comida: " << "Peixe" << endl;
-    cout << "Kg consumidos: " << kgPeixeConsumidos01+kgPeixeConsumidos03<<endl;
+    cout << "Kg consumidos: " << animal01.getQuantidadeConsumida() + animal05.getQuantidadeConsumida() <<endl;
 
     return 0;
 }
