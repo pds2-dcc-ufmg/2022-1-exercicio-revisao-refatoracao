f22fac806ccbe34e494fffdfd463bfabdf79d404
Bernardonasc <95573525+Bernardonasc@users.noreply.github.com>
2022-07-01 18:34:46 -0300

    Update Cuidador.cpp

--- Cuidador.cpp
+++ Cuidador.cpp
@@ -1,14 +1,5 @@
-#include <iostream>
-#include <string>
 #include "Cuidador.hpp"
 
-void Cuidador::print(){
-
-	std::cout << "[Cuidador]" << endl
-         << "  Nome: " << NOME << endl
-		 << "  CPF: " << cpf << endl
-		<< "  Telefone: " << Telefone << endl
-		 << "  Nascimento: " << BIRTH << endl;
-
+Cuidador::Cuidador(std::string _nome) {
+    this->nome = _nome;
 }
-
