244d8061a9fcda041ba0b0feee5384e80f8f874f
BrunaMilione <88358572+BrunaMilione@users.noreply.github.com>
2022-07-01 23:42:18 -0300

    Update Alimentacao.hpp
    Foi tornando privado atributos das classes, comentado os códigos e colocados as implementações das classe no arquivo.cpp separado

--- Alimentacao.hpp
+++ Alimentacao.hpp
@@ -6,36 +6,44 @@
 
 using namespace std;
 
-class Alimentacao{
-
-    public:
+//Classe de alimentação de um animal
+class Alimentacao {
+private:
+    //Tamanho da porcao
     int porcao;
+    //Nome da comida
     string comida;
+    //Descricao da comida
     string descricao;
+    //Objeto cuidador do animal
     Cuidador cuid;
+    //Nome do animal
     string nomeAnimal;
 
+public:
+    //Funcao Get para Porcao
+    int getPorcao() {return porcao;};
+    //Funcao Set para Porcao
+    void setPorcao(int porcao_) {porcao = porcao_;};
+    //Funcao Get para Comida
+    string getComida() {return comida;};
+    //Funcao Set para Comida
+    void setComida(string comida_) {comida = comida_;};
+    //Funcao Get para Descricao
+    string getDescricao() {return descricao;};
+    //Funcao Set para Descricao
+    void setDescricao(string descricao_) {descricao = descricao_;};
+    //Funcao Get para Porcao
+    Cuidador getCuidador() {return cuid;};
+    //Funcao Set para Cuidador
+    void setCuidador(Cuidador cuid_) {cuid = cuid_;};
+    //Funcao Get para Nome do Animal
+    string getnomeAnimal() {return nomeAnimal;};
+    //Funcao Set para Cuidador
+    void setnomeAnimal(string nomeAnimal_) {nomeAnimal = nomeAnimal_;};
 
-    void print() {
-
-        std::cout << "Animal: ";
-        cout << nomeAnimal << "\n";
-
-        std::cout << " Cuidador: ";
-        cout << cuid.NOME << "\n";
-
-        //std::cout << " : ";
-        //cout << ursidae;
-    }
-
-    /*void print() {
-
-        std::cout << "Cuidador: ";
-        cout << cuida;
-
-        std::cout << " Animal: ";
-        cout << alimentado;
-    }*/
+    //Funcao para imprimir as informações do animal e seu cuidado
+    void print();
 };
 
 #endif
