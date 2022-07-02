33fce2289e7f006bad49b69e709bfd51bc465927
Rafaela Alexandre <85119132+hydrogradCreator@users.noreply.github.com>
2022-07-01 23:01:49 -0300

    Herpestidae.hpp Modificado

--- Herpestidae.hpp
+++ Herpestidae.hpp
@@ -1,35 +1,19 @@
 #ifndef Herpestidae_HPP
 #define Herpestidae_HPP
-
 #include "Animal.hpp"
-
-using namespace std;
-
-double quantPORCAO = 1;
-
 
 class Herpestidae : public Animal {
 
-    public:
+    private:
+
+        std::string familia, nome;
+        int idade;
+        std::string especie;
 
     public:
-  string species;
 
-    double kgConsumidosDeRacao(int porcoesConsumidas) {
-        double c = quantPORCAO*porcoesConsumidas;
-                  return c;
-    }
-
-
-    void print() {
-
-        Animal::print();
-
-
-
-        std::cout << "  Especie: " << species <<endl;
-
-    }
+        Herpestidae(std::string nome, int idade, std::string familia, std::string especie);
+        void print() override;
 };
 
 #endif
