31e25653230d7b09f74619db4d41c814d302c867
Rafaela Alexandre <85119132+hydrogradCreator@users.noreply.github.com>
2022-07-01 22:59:57 -0300

    Implementacao - Cuidador.cpp

--- Cuidador.cpp
+++ Cuidador.cpp
@@ -2,13 +2,25 @@
 #include <string>
 #include "Cuidador.hpp"
 
+Cuidador::Cuidador(std::string _nome, std::string _cpf, std::string _telefone, std::string _birth,std::string _salario){
+	this->nome = _nome;
+	this->cpf = _cpf;
+	this->telefone = _telefone;
+	this->birth = _birth;
+	this->salario = _salario;
+}
+
+
 void Cuidador::print(){
 
-	std::cout << "[Cuidador]" << endl
-         << "  Nome: " << NOME << endl
-		 << "  CPF: " << cpf << endl
-		<< "  Telefone: " << Telefone << endl
-		 << "  Nascimento: " << BIRTH << endl;
+	std::cout << "[Cuidador]" << std::endl;
+	std::cout << "  Nome: " << this->nome << std::endl;
+	std::cout << "  CPF: " << this->cpf << std::endl;
+	std::cout << "  Telefone: " << this->telefone << std::endl << "  Nascimento: " << this->birth << std::endl;
 
+}
+
+std::string Cuidador::getNomeCuidador(){
+	return this->nome;
 }
 
