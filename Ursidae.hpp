87e6707cf1a99d30efe9c701189944f42bc75a47
matheusathan <103614259+matheusathan@users.noreply.github.com>
2022-07-03 18:42:58 -0400

    Update Ursidae.hpp
    Correção do modificador de acesso

--- Ursidae.hpp
+++ Ursidae.hpp
@@ -7,12 +7,14 @@
 const int URSIDAE_PEIXES_POR_PORCAO = 5;
 
 class Ursidae {
-    public:
-
+    
     int idade;
     std::string nome;
     std::string especie;
     double kgPeixePorDia;
+    
+    public:
+    
     Ursidae(int _idade, std::string _nome, std::string _especie, double _kgPeixePorDia);
 
     void print();
